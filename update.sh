d12c16986dc372700b0d9c1504356b63f946ff7f
Jake Jarvis <jake@jarv.is>
2020-02-29 15:51:20 -0500

    Remove CLI variants and simplify travis builds

--- update.sh
+++ update.sh
@@ -10,25 +10,6 @@ sha1="$(curl -fsSL "https://wordpress.org/wordpress-$current.tar.gz.sha1")"
 
 phpVersions=( php*.* )
 
-cliPossibleVersions=( $(
-	git ls-remote --tags 'https://github.com/wp-cli/wp-cli.git' \
-		| sed -r 's!^[^\t]+\trefs/tags/v([^^]+).*!\1!g' \
-		| sort --version-sort --reverse
-	)
-)
-cliVersion=
-cliSha512=
-for cliPosVer in "${cliPossibleVersions[@]}"; do
-	cliUrl="https://github.com/wp-cli/wp-cli/releases/download/v${cliPosVer}/wp-cli-${cliPosVer}.phar.sha512"
-	if cliSha512="$(curl -fsSL "$cliUrl" 2>/dev/null)"; then
-		cliVersion="$cliPosVer"
-		break
-	fi
-done
-echo "$current (CLI '$cliVersion')"
-# make sure we get a cliVersion
-[ -n "$cliVersion" ]
-
 declare -A variantExtras=(
 	[apache]="$(< apache-extras.template)"
 )
@@ -36,13 +17,11 @@ declare -A variantCmds=(
 	[apache]='apache2-foreground'
 	[fpm]='php-fpm'
 	[fpm-alpine]='php-fpm'
-	[cli]='' # unused
 )
 declare -A variantBases=(
 	[apache]='debian'
 	[fpm]='debian'
 	[fpm-alpine]='alpine'
-	[cli]='cli'
 )
 
 sed_escape_rhs() {
@@ -54,7 +33,7 @@ for phpVersion in "${phpVersions[@]}"; do
 	phpVersionDir="$phpVersion"
 	phpVersion="${phpVersion#php}"
 
-	for variant in apache fpm fpm-alpine cli; do
+	for variant in apache fpm fpm-alpine; do
 		dir="$phpVersionDir/$variant"
 		mkdir -p "$dir"
 
@@ -66,17 +45,12 @@ for phpVersion in "${phpVersions[@]}"; do
 		base="${variantBases[$variant]}"
 
 		entrypoint='docker-entrypoint.sh'
-		if [ "$variant" = 'cli' ]; then
-			entrypoint='cli-entrypoint.sh'
-		fi
 
 		sed -r \
 			-e 's!%%WORDPRESS_VERSION%%!'"$current"'!g' \
 			-e 's!%%WORDPRESS_SHA1%%!'"$sha1"'!g' \
 			-e 's!%%PHP_VERSION%%!'"$phpVersion"'!g' \
 			-e 's!%%VARIANT%%!'"$variant"'!g' \
-			-e 's!%%WORDPRESS_CLI_VERSION%%!'"$cliVersion"'!g' \
-			-e 's!%%WORDPRESS_CLI_SHA512%%!'"$cliSha512"'!g' \
 			-e 's!%%VARIANT_EXTRAS%%!'"$(sed_escape_rhs "$extras")"'!g' \
 			-e 's!%%CMD%%!'"$cmd"'!g' \
 			"Dockerfile-${base}.template" > "$dir/Dockerfile"
